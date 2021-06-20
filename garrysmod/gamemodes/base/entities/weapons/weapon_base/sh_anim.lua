790f17882a5558a6a7fb6ebd7ce4018499b9f09e
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-06-20 20:59:02 +0100

    sh_anim optimisation
    self.Owner -> self:GetOwner()
    direct table input upon creation

--- garrysmod/gamemodes/base/entities/weapons/weapon_base/sh_anim.lua
+++ garrysmod/gamemodes/base/entities/weapons/weapon_base/sh_anim.lua
@@ -37,19 +37,20 @@ function SWEP:SetWeaponHoldType( t )
 		index = ActIndex[ t ]
 	end
 
-	self.ActivityTranslate = {}
-	self.ActivityTranslate[ ACT_MP_STAND_IDLE ]					= index
-	self.ActivityTranslate[ ACT_MP_WALK ]						= index + 1
-	self.ActivityTranslate[ ACT_MP_RUN ]						= index + 2
-	self.ActivityTranslate[ ACT_MP_CROUCH_IDLE ]				= index + 3
-	self.ActivityTranslate[ ACT_MP_CROUCHWALK ]					= index + 4
-	self.ActivityTranslate[ ACT_MP_ATTACK_STAND_PRIMARYFIRE ]	= index + 5
-	self.ActivityTranslate[ ACT_MP_ATTACK_CROUCH_PRIMARYFIRE ]	= index + 5
-	self.ActivityTranslate[ ACT_MP_RELOAD_STAND ]				= index + 6
-	self.ActivityTranslate[ ACT_MP_RELOAD_CROUCH ]				= index + 6
-	self.ActivityTranslate[ ACT_MP_JUMP ]						= index + 7
-	self.ActivityTranslate[ ACT_RANGE_ATTACK1 ]					= index + 8
-	self.ActivityTranslate[ ACT_MP_SWIM ]						= index + 9
+	self.ActivityTranslate = {
+		ACT_MP_STAND_IDLE, 					= index
+		ACT_MP_WALK, 						= index + 1
+		ACT_MP_RUN, 						= index + 2
+		ACT_MP_CROUCH_IDLE, 					= index + 3
+		ACT_MP_CROUCHWALK, 					= index + 4
+		ACT_MP_ATTACK_STAND_PRIMARYFIRE, 			= index + 5
+		ACT_MP_ATTACK_CROUCH_PRIMARYFIRE, 			= index + 5
+		ACT_MP_RELOAD_STAND, 					= index + 6
+		ACT_MP_RELOAD_CROUCH, 					= index + 6
+		ACT_MP_JUMP, 						= index + 7
+		ACT_RANGE_ATTACK1, 					= index + 8
+		ACT_MP_SWIM, 						= index + 9
+	}
 
 	-- "normal" jump animation doesn't exist
 	if ( t == "normal" ) then
@@ -71,7 +72,7 @@ SWEP:SetWeaponHoldType( "pistol" )
 -----------------------------------------------------------]]
 function SWEP:TranslateActivity( act )
 
-	if ( self.Owner:IsNPC() ) then
+	if ( self:GetOwner:IsNPC() ) then
 		if ( self.ActivityTranslateAI[ act ] ) then
 			return self.ActivityTranslateAI[ act ]
 		end
